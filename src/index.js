bfa905f655e813401e3637d36c4c117ac4c6d7bc
Gianmarco Pow-Sang <hello@gpstech.io>
2022-08-26 23:17:57 -0400

    added react routing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+//BrowserRouter leverages the URL in order to keep track of the history of there the user is navigating through
+import { BrowserRouter } from "react-router-dom";
+import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* nesting the app in BrowserRouter allows the components to connect to the URL and render the appropriate elements*/}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,28 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+
+import "./navigation.styles.scss";
+
+function Navigation() {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          {/* Link is essentially an anchor tag that appropriately dynamically uses the correct browser you have installed*/}
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      {/* we are using the outlet to render our components inside our navigation*/}
+      <Outlet />
+    </Fragment>
+  );
+}
+
+export default Navigation;
