e31dfaf974edb858464e4cfa5b77f49bc4d7ced0
Gianmarco Pow-Sang <hello@gpstech.io>
2022-08-31 21:07:09 -0400

    added functionality to add items to cart

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -1,13 +1,43 @@
 import { createContext, useState } from "react";
 
+//helper function that helps find in existing array any cart item that matches item added
+
+const addCartItem = (cartItems, productToAdd) => {
+  //find if product does not exist in cart
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
+
+  //if product already exists in cart, increment quantity
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  //return new array with modified cartItems/ new cart item
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = { isCartOpen, setIsCartOpen };
+  const [cartItems, setCartItems] = useState([]);
+
+  //triggers whenever a user presses Add to Cart
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+
+  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
