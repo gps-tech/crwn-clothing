618012b6b01d6ade9d017e764ec9799d0d07647e
Gianmarco Pow-Sang <hello@gpstech.io>
2022-09-01 23:57:42 -0400

    added om addToCart feature

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,11 +1,16 @@
 import "./cart-item.style.scss";
 
 function CartItem({ CartItem }) {
-  const { name, quantity } = CartItem;
+  const { name, imageUrl, price, quantity } = CartItem;
   return (
-    <div>
-      <h2>{name}</h2>
-      <span>{quantity}</span>
+    <div className="cart-item-container">
+      <img src={imageUrl} alt={`${name}`} />
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className="price">
+          {quantity} x ${price}
+        </span>
+      </div>
     </div>
   );
 }
