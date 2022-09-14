dc553cb0f7cf1383b16086252f47c5c985ac6e67
Gianmarco Pow-Sang <hello@gpstech.io>
2022-09-13 18:50:57 -0600

    updated some routing for categories and updated some styling

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,4 +1,4 @@
-import CategoryItem from "../category-item/category-item.component.jsx";
+import DirectoryItem from "../directory-item/directory-item.component";
 import "./directory.styes.scss";
 
 import React from "react";
@@ -7,7 +7,7 @@ function Directory({ categories }) {
   return (
     <div className="directory-container">
       {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
+        <DirectoryItem key={category.id} category={category} />
       ))}
     </div>
   );
